b52cef4a8d64b93c96b7f1c6844f113a5ab235aa
MDrFaza <mdrfaza21@gmail.com>
2021-09-30 19:46:46 +0700

    save all user data local finished

--- dilo-incrementalgamefirebase-preparation/Assets/Scripts/UserDataManager.cs
+++ dilo-incrementalgamefirebase-preparation/Assets/Scripts/UserDataManager.cs
@@ -29,4 +29,9 @@ public class UserDataManager
         string json = JsonUtility.ToJson (Progress);
         PlayerPrefs.SetString (PROGRESS_KEY, json);
     }
+
+    public static bool HasResources (int index)
+    {
+        return index + 1 <= Progress.ResourcesLevels.Count;
+    }
 }
