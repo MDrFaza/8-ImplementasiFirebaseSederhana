f72fb899ff6a074fa7b84f3b34826aa2147e4e72
MDrFaza <mdrfaza21@gmail.com>
2021-10-01 21:38:11 +0700

    setup firebase and analytics finished

--- dilo-incrementalgamefirebase-preparation/Assets/Scripts/UserDataManager.cs
+++ dilo-incrementalgamefirebase-preparation/Assets/Scripts/UserDataManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Firebase.Storage;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class UserDataManager
@@ -7,12 +9,13 @@ public class UserDataManager
     private const string PROGRESS_KEY = "Progress";
     public static UserProgressData Progress;
 
-    public static void Load ()
+    public static void LoadFromLocal ()
     {
         // Cek apakah ada data yang tersimpan sebagai PROGRESS_KEY
         if (!PlayerPrefs.HasKey (PROGRESS_KEY))
         {
             // Jika tidak ada, maka buat data baru
+            // dan upload ke Cloud
             Progress = new UserProgressData ();
             Save ();
         }
@@ -24,10 +27,63 @@ public class UserDataManager
         }
     }
 
-    public static void Save ()
+    public static IEnumerator LoadFromCloud (System.Action onComplete)
+    {
+        StorageReference targetStorage = GetTargetCloudStorage ();
+
+        bool isCompleted = false;
+        bool isSuccessfull = false;
+        const long maxAllowedSize = 1024 * 1024; // Sama dengan 1 MB
+
+        targetStorage.GetBytesAsync (maxAllowedSize).ContinueWith ((Task<byte[]> task) =>
+        {
+            if (!task.IsFaulted)
+            {
+                string json = Encoding.Default.GetString (task.Result);
+                Progress = JsonUtility.FromJson<UserProgressData> (json);
+                isSuccessfull = true;
+            }
+            isCompleted = true;
+        });
+
+        while (!isCompleted)
+        {
+            yield return null;
+        }
+
+        // Jika sukses mendownload, maka simpan data hasil download
+        if (isSuccessfull)
+        {
+            Save ();
+        }
+        else
+        {
+            // Jika tidak ada data di cloud, maka load data dari local
+            LoadFromLocal ();
+        }
+        onComplete?.Invoke ();
+    }
+
+    public static void Save (bool uploadToCloud = false)
     {
         string json = JsonUtility.ToJson (Progress);
         PlayerPrefs.SetString (PROGRESS_KEY, json);
+
+        if (uploadToCloud)
+        {
+            AnalyticsManager.SetUserProperties ("gold", Progress.Gold.ToString ());
+            byte[] data = Encoding.Default.GetBytes (json);
+            StorageReference targetStorage = GetTargetCloudStorage ();
+            targetStorage.PutBytesAsync (data);
+        }
+    }
+
+    private static StorageReference GetTargetCloudStorage ()
+    {
+        // Gunakan Device ID sebagai nama file yang akan disimpan di cloud
+        string deviceID = SystemInfo.deviceUniqueIdentifier;
+        FirebaseStorage storage = FirebaseStorage.DefaultInstance;
+        return storage.GetReferenceFromUrl ($"{storage.RootReference}/{deviceID}");
     }
 
     public static bool HasResources (int index)
